c4652436cccfb607b944490ea294b432a39e853c
George Adams <georgeadams1995@gmail.com>
2025-01-07 14:40:08 +0000

    convert big to byteslice

--- bbig/big.go
+++ bbig/big.go
@@ -10,11 +10,22 @@ import (
 )
 
 func Enc(b *big.Int) xcrypto.BigInt {
-	// Return the input directly since BigInt is now *big.Int
-	return b
+	if b == nil {
+		return nil
+	}
+	x := b.Bytes()
+	if len(x) == 0 {
+		return xcrypto.BigInt{}
+	}
+	return x
 }
 
 func Dec(b xcrypto.BigInt) *big.Int {
-	// Return the input directly since BigInt is now *big.Int
-	return b
+	if b == nil {
+		return nil
+	}
+	if len(b) == 0 {
+		return new(big.Int)
+	}
+	return new(big.Int).SetBytes(b)
 }

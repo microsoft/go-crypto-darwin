87efe181175e71697244aba250511b7dc0396eb4
mertakman <mertakman2012@gmail.com>
2025-03-17 03:33:09 +0000

    fix: correctly handle nil case

--- internal/cryptokit/hash.go
+++ internal/cryptokit/hash.go
@@ -10,32 +10,52 @@ import "C"
 import "unsafe"
 
 func SHA1(p []byte) (sum [20]byte) {
+	ptr := unsafe.Pointer(nil)
+	if len(p) > 0 {
+		ptr = unsafe.Pointer(&p[0])
+	}
+
 	C.SHA1(
-		(*C.uint8_t)(unsafe.Pointer(&p[0])),
+		(*C.uint8_t)(ptr),
 		C.size_t(len(p)),
 		(*C.uint8_t)(unsafe.Pointer(&sum[0])))
 	return
 }
 
 func SHA256(p []byte) (sum [32]byte) {
+	ptr := unsafe.Pointer(nil)
+	if len(p) > 0 {
+		ptr = unsafe.Pointer(&p[0])
+	}
+
 	C.SHA256(
-		(*C.uint8_t)(unsafe.Pointer(&p[0])),
+		(*C.uint8_t)(ptr),
 		C.size_t(len(p)),
 		(*C.uint8_t)(unsafe.Pointer(&sum[0])))
 	return
 }
 
 func SHA384(p []byte) (sum [48]byte) {
+	ptr := unsafe.Pointer(nil)
+	if len(p) > 0 {
+		ptr = unsafe.Pointer(&p[0])
+	}
+
 	C.SHA384(
-		(*C.uint8_t)(unsafe.Pointer(&p[0])),
+		(*C.uint8_t)(ptr),
 		C.size_t(len(p)),
 		(*C.uint8_t)(unsafe.Pointer(&sum[0])))
 	return
 }
 
 func SHA512(p []byte) (sum [64]byte) {
+	ptr := unsafe.Pointer(nil)
+	if len(p) > 0 {
+		ptr = unsafe.Pointer(&p[0])
+	}
+
 	C.SHA512(
-		(*C.uint8_t)(unsafe.Pointer(&p[0])),
+		(*C.uint8_t)(ptr),
 		C.size_t(len(p)),
 		(*C.uint8_t)(unsafe.Pointer(&sum[0])))
 	return

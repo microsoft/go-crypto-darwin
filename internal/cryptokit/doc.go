4f06779afbf6aa89b06cac9651fe16db3ffba6ff
qmuntal <qmuntaldiaz@microsoft.com>
2025-04-14 10:30:08 +0200

    add missing cgo constraint

--- internal/cryptokit/doc.go
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-//go:build darwin
-
-// The cryptokit package provides cryptographic functions using Apple's CryptoKit.
-package cryptokit

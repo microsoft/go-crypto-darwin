f90c0032512c43bbd4b26a2eb5727ac3da874b81
George Adams <georgeadams1995@gmail.com>
2025-08-28 09:54:34 +0100

    fix gen-swift-bindings.sh on newer macos systems

--- gen-swift-bindings.sh
+++ gen-swift-bindings.sh
@@ -13,6 +13,11 @@ rm -rf .build
 echo "Building Swift bindings..."
 xcrun swift build -c release --arch arm64 --arch x86_64
 
-cp .build/apple/Products/Release/CryptoKitSrc.o ../internal/cryptokit/CryptoKit.o
+# on macOS 15+ it generates a CryptoKitSrc_Module.o
+if [ -f .build/apple/Products/Release/CryptoKitSrc_Module.o ]; then
+    cp .build/apple/Products/Release/CryptoKitSrc_Module.o ../internal/cryptokit/CryptoKit.o
+else [ -f .build/apple/Products/Release/CryptoKitSrc.o ];
+    cp .build/apple/Products/Release/CryptoKitSrc.o ../internal/cryptokit/CryptoKit.o
+fi
 
 echo "Build complete."

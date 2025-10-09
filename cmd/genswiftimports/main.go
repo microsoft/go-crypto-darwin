3b679c46516e6809a0802fdb4f864a0c18447c72
qmuntal <quimmuntal@gmail.com>
2025-10-09 13:31:58 +0200

    fix genswiftimports

--- cmd/genswiftimports/main.go
+++ cmd/genswiftimports/main.go
@@ -112,6 +112,11 @@ func main() {
 		// create final lines
 		finalLines := []string{}
 		for _, name := range missing {
+			if name == "syscall.syscallN" {
+				// syscall.syscallN is added in go 1.26, so it can appear as a missing symbol
+				// when running this tool with go 1.25. Ignore it.
+				continue
+			}
 			dylib := symbols[name]
 			finalLines = append(finalLines, fmt.Sprintf("//go:cgo_import_dynamic %s %s \"%s\"", name, name, dylib))
 		}

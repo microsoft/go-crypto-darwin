f3cc230aca2432570f9ec4d4dacde3224de9c36d
qmuntal <quimmuntal@gmail.com>
2025-10-09 13:15:48 +0200

    shell out to the mkcgo implementation in the openssl backend

--- internal/cryptokit/cryptokit.go
+++ internal/cryptokit/cryptokit.go
@@ -1,14 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-//go:build cgo && darwin
+//go:build darwin
 
 package cryptokit
 
 //go:generate go run ../../cmd/mkcgo -out zcryptokit.go -package cryptokit --noerrors shims.h
 //go:generate go run ../../cmd/mkcgo -out zcryptokit.go -nocgo -package cryptokit --noerrors shims.h
 //go:generate go run ../../cmd/genswiftimports
-
-// #cgo CFLAGS: -Wno-deprecated-declarations
-// #cgo LDFLAGS: -framework Security -framework CoreFoundation
-import "C"

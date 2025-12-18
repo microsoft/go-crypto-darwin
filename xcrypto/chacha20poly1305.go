dfd915c7ec1a7c508b11b7a988a60fced2da7196
George Adams <georgeadams1995@gmail.com>
2025-12-18 11:52:04 +0000

    Quim review fixes

--- xcrypto/chacha20poly1305.go
+++ xcrypto/chacha20poly1305.go
@@ -13,28 +13,18 @@ import (
 )
 
 const (
-	// KeySize is the size of the key used by this AEAD, in bytes.
-	chachaKeySize = 32
-
-	// NonceSize is the size of the nonce used with the standard variant of this
-	// AEAD, in bytes.
-	//
-	// Note that this is too short to be safely generated at random if the same
-	// key is reused more than 2³² times.
-	chachaNonceSize = 12
-
-	// Overhead is the size of the Poly1305 authentication tag, and the
-	// difference between a ciphertext length and its plaintext.
-	chachaOverhead = 16
+	chacha20Poly1305KeySize   = 32
+	chacha20Poly1305NonceSize = 12
+	chacha20Poly1305Overhead  = 16
 )
 
 type chacha20poly1305 struct {
-	key [chachaKeySize]byte
+	key [chacha20Poly1305KeySize]byte
 }
 
 // NewChaCha20Poly1305 returns a ChaCha20-Poly1305 AEAD that uses the given 256-bit key.
 func NewChaCha20Poly1305(key []byte) (cipher.AEAD, error) {
-	if len(key) != chachaKeySize {
+	if len(key) != chacha20Poly1305KeySize {
 		return nil, errors.New("chacha20poly1305: bad key length")
 	}
 	ret := new(chacha20poly1305)
@@ -43,63 +33,56 @@ func NewChaCha20Poly1305(key []byte) (cipher.AEAD, error) {
 }
 
 func (c *chacha20poly1305) NonceSize() int {
-	return chachaNonceSize
+	return chacha20Poly1305NonceSize
 }
 
 func (c *chacha20poly1305) Overhead() int {
-	return chachaOverhead
+	return chacha20Poly1305Overhead
 }
 
 func (c *chacha20poly1305) Seal(dst, nonce, plaintext, additionalData []byte) []byte {
-	if len(nonce) != chachaNonceSize {
+	if len(nonce) != chacha20Poly1305NonceSize {
 		panic("chacha20poly1305: bad nonce length passed to Seal")
 	}
-
 	if uint64(len(plaintext)) > (1<<38)-64 {
 		panic("chacha20poly1305: plaintext too large")
 	}
-
-	return c.seal(dst, nonce, plaintext, additionalData)
+	ret, out := sliceForAppend(dst, len(plaintext)+chacha20Poly1305Overhead)
+	if inexactOverlap(out, plaintext) {
+		panic("chacha20poly1305: invalid buffer overlap of output and input")
+	}
+	if anyOverlap(out, additionalData) {
+		panic("chacha20poly1305: invalid buffer overlap of output and additional data")
+	}
+	tag := out[len(out)-chacha20Poly1305Overhead:]
+	if cryptokit.EncryptChaChaPoly(c.key[:], plaintext, nonce, additionalData, out[:len(plaintext)], tag) != 0 {
+		panic("chacha20poly1305: encryption failed")
+	}
+	return ret
 }
 
-var errOpenChaCha = errors.New("chacha20poly1305: message authentication failed")
-
 func (c *chacha20poly1305) Open(dst, nonce, ciphertext, additionalData []byte) ([]byte, error) {
-	if len(nonce) != chachaNonceSize {
+	if len(nonce) != chacha20Poly1305NonceSize {
 		panic("chacha20poly1305: bad nonce length passed to Open")
 	}
 	if len(ciphertext) < 16 {
-		return nil, errOpenChaCha
+		return nil, errOpen
 	}
 	if uint64(len(ciphertext)) > (1<<38)-48 {
 		panic("chacha20poly1305: ciphertext too large")
 	}
-
-	return c.open(dst, nonce, ciphertext, additionalData)
-}
-
-func (c *chacha20poly1305) seal(dst, nonce, plaintext, additionalData []byte) []byte {
-	ret, out := sliceForAppend(dst, len(plaintext)+chachaOverhead)
-
-	tag := out[len(plaintext):]
-
-	if cryptokit.EncryptChaChaPoly(c.key[:], plaintext, nonce, additionalData, out[:len(plaintext)], tag) != 0 {
-		panic("chacha20poly1305: encryption failed")
+	tag := ciphertext[len(ciphertext)-chacha20Poly1305Overhead:]
+	ciphertext = ciphertext[:len(ciphertext)-chacha20Poly1305Overhead]
+	ret, out := sliceForAppend(dst, len(ciphertext))
+	if inexactOverlap(out, ciphertext) {
+		panic("chacha20poly1305: invalid buffer overlap of output and input")
 	}
-
-	return ret
-}
-
-func (c *chacha20poly1305) open(dst, nonce, ciphertext, additionalData []byte) ([]byte, error) {
-	tag := ciphertext[len(ciphertext)-16:]
-	ct := ciphertext[:len(ciphertext)-16]
-
-	ret, out := sliceForAppend(dst, len(ct))
-
+	if anyOverlap(out, additionalData) {
+		panic("chacha20poly1305: invalid buffer overlap of output and additional data")
+	}
 	var outLen int
-	if cryptokit.DecryptChaChaPoly(c.key[:], ct, nonce, additionalData, tag, out, &outLen) != 0 {
-		return nil, errOpenChaCha
+	if cryptokit.DecryptChaChaPoly(c.key[:], ciphertext, nonce, additionalData, tag, out, &outLen) != 0 {
+		return nil, errOpen
 	}
-
 	return ret[:len(dst)+outLen], nil
 }
